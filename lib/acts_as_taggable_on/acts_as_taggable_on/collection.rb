be8bfc1edfb667ad2291e9fea9275f307e7adf19
hirosuke768 <hirosuke768@gmail.com>
2013-03-08 13:57:58 +0900

    Update collection.rb
    Remove "AS" before table alias.

--- lib/acts_as_taggable_on/acts_as_taggable_on/collection.rb
+++ lib/acts_as_taggable_on/acts_as_taggable_on/collection.rb
@@ -90,7 +90,7 @@ module ActsAsTaggableOn::Taggable
         tagging_scope = tagging_scope.where("#{ActsAsTaggableOn::Tagging.table_name}.taggable_id IN(#{select(scoped_select).to_sql})").
                                       group(group_columns)
 
-        tag_scope = tag_scope.joins("JOIN (#{tagging_scope.to_sql}) AS #{ActsAsTaggableOn::Tagging.table_name} ON #{ActsAsTaggableOn::Tagging.table_name}.tag_id = #{ActsAsTaggableOn::Tag.table_name}.id")
+        tag_scope = tag_scope.joins("JOIN (#{tagging_scope.to_sql}) #{ActsAsTaggableOn::Tagging.table_name} ON #{ActsAsTaggableOn::Tagging.table_name}.tag_id = #{ActsAsTaggableOn::Tag.table_name}.id")
         tag_scope
       end
       
@@ -169,7 +169,7 @@ module ActsAsTaggableOn::Taggable
                                       having(having)
 
 
-        tag_scope = tag_scope.joins("JOIN (#{tagging_scope.to_sql}) AS #{ActsAsTaggableOn::Tagging.table_name} ON #{ActsAsTaggableOn::Tagging.table_name}.tag_id = #{ActsAsTaggableOn::Tag.table_name}.id")
+        tag_scope = tag_scope.joins("JOIN (#{tagging_scope.to_sql}) #{ActsAsTaggableOn::Tagging.table_name} ON #{ActsAsTaggableOn::Tagging.table_name}.tag_id = #{ActsAsTaggableOn::Tag.table_name}.id")
         tag_scope
       end
     end
